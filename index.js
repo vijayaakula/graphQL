888e17d7c08ddc1a3f713f312feb676f9fdf0eb3
vijaya <vijaya.akula@krify.com>
2025-08-04 12:12:15 +0530

    2nd commit in dev1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello World")
+console.log("This is a simple Node.js application.");
+console.log("It prints messages to the console.");
+console.log("You can modify this code to add more functionality.");
+console.log("Have a great day!");
+console.log("Remember to save your changes before running the application again.");     
