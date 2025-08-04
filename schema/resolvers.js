a4acdfa2a9686025794544e1b4371e2e9b53a50e
vijaya <vijaya.akula@krify.com>
2025-08-04 14:53:51 +0530

    updateUser commit 1

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -16,8 +16,18 @@ export const resolvers ={
     createUser: async (_, args) => {
       const { name, email } = args;
       return await db.User.create({ name, email });
- 
- }
+    },
+    updateUser: async (_, args) => {
+      const { id, name, email } = args;
+      const user = await db.User.findByPk(id);
+      if (user) {
+        user.name = name || user.name;
+        user.email = email || user.email;
+        await user.save();
+        return user;
+      }
+      throw new Error('User not found');
+    }
 }
 
 }
